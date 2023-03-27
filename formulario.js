a1dd1ab71312a99d480b4cd293d229e27f08228a
miribg <bergazm@gmail.com>
2023-03-28 01:09:32 +0200

    validacion comenzar

---
+++ formulario.js
@@ -0,0 +1,12 @@
+//var btnEnviar = document.getElementById("enviar");
+
+function validacion(){
+    var textNomb = document.getElementById("campo_nombre").value;
+    var textLib = document.getElementById("campo_libros").value;
+    if (textNomb=='' ){
+        console.log('error: vacio');
+        alert("Error, vacio");
+        return false;
+    }
+    return true;
+}
