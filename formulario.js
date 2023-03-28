4bb6c347f25d57f7186e5981cc02f53809ba1a14
Julen <jfuentes019@ikasle.ehu.eus>
2023-03-28 14:42:41 +0200

    Cambiar tipo boton

--- formulario.js
+++ formulario.js
@@ -3,7 +3,7 @@
 function validacion(){
     var textNomb = document.getElementById("campo_nombre").value;
     var textLib = document.getElementById("campo_libros").value;
-    if (textNomb=='' ){
+    if (textNomb === '' ){
         console.log('error: vacio');
         alert("Error, vacio");
         return false;
