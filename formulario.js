2517b6b9f6486239469a972c4871769c63d50205
Julen <jfuentes019@ikasle.ehu.eus>
2023-03-28 15:49:59 +0200

    Validacion libros hecho

--- formulario.js
+++ formulario.js
@@ -5,17 +5,43 @@ function validacion(){
     var textLib = document.getElementById("campo_libros").value;
 
     let campoNombre = document.getElementById("campo_nombre");
-    validarNombreCliente(campoNombre);
+    let campoLibros = document.getElementById("campo_libros");
+    let divLibros = document.getElementById("div_libros");
+    let campoCantidadLibros = document.getElementById("campo_cantidad");
+
+    validarCampoNombre(campoNombre);
+    let pErrorLibros = validarCampoLibros(campoLibros, divLibros);
+    validarCampoCantidadLibros(campoCantidadLibros, pErrorLibros);
 
     return textNomb !== '';
-
 }
 
-function validarNombreCliente(pCampoNombre) {
+function validarCampoNombre(pCampoNombre) {
     if (pCampoNombre.value === "") {
-        console.log("Campo Nombre VACÍO");
         let avisoNombre = document.createElement("p");
         avisoNombre.innerHTML = "El campo del Nombre es obligatorio";
+        avisoNombre.style.color = "orange";
         pCampoNombre.insertAdjacentElement("afterend", avisoNombre);
+    }
+}
+
+function validarCampoLibros(pCampoLibros, pDivLibros) {
+    if (pCampoLibros.value === "") {
+        let avisoLibros = document.createElement("p");
+        avisoLibros.innerHTML = "- El campo del Libro es obligatorio";
+        avisoLibros.style.color = "orange";
+        pDivLibros.insertAdjacentElement("afterend", avisoLibros);
+        return avisoLibros;
+    }
+    return null;
+}
+
+function validarCampoCantidadLibros(pCampoCantidad, pErrorLibros) {
+    if (pCampoCantidad.value < 1 || pCampoCantidad.value > 5) {
+        console.log("Cantidad incorrecta");
+        let avisoCantidadLibros = document.createElement("p");
+        avisoCantidadLibros.innerHTML = "- La cantidad debe ser un número del 1 al 5";
+        avisoCantidadLibros.style.color = "orange";
+        pErrorLibros.insertAdjacentElement("afterend", avisoCantidadLibros);
     }
 }
