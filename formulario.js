50ae5a186d99d323cb3c6a1e2bb835d63a6b86a9
Julen <jfuentes019@ikasle.ehu.eus>
2023-03-28 20:31:02 +0200

    Validaciones con aviso HECHO

--- formulario.js
+++ formulario.js
@@ -1,73 +1,123 @@
-//var btnEnviar = document.getElementById("enviar");
-
 function validacion(){
-    var textNomb = document.getElementById("campo_nombre").value;
-    var textLib = document.getElementById("campo_libros").value;
-
     let campoNombre = document.getElementById("campo_nombre");
+    let campoTel = document.getElementById("campo_telefono");
+    let campoEmail = document.getElementById("campo_email");
     let campoLibros = document.getElementById("campo_libros");
     let divLibros = document.getElementById("div_libros");
     let campoCantidadLibros = document.getElementById("campo_cantidad");
-    let campoTel= document.getElementById("campo_telefono");
-    let campoEmail= document.getElementById("campo_email");
 
-    validarCampoNombre(campoNombre);
-    validarCampoTel(campoTel);
-    validarCampoEmail(campoEmail);
-    let pErrorLibros = validarCampoLibros(campoLibros, divLibros);
-    validarCampoCantidadLibros(campoCantidadLibros, pErrorLibros);
+    let nombreValido = validarCampoNombre(campoNombre);
+    let telValido = validarCampoTel(campoTel);
+    let emailValido = validarCampoEmail(campoEmail);
+    let librosValidos = validarCampoLibros(campoLibros, divLibros);
+    let cantidadLibrosValido = validarCampoCantidadLibros(campoCantidadLibros, librosValidos, divLibros);
 
-    return textNomb !== '';
+    if (nombreValido && telValido && emailValido && librosValidos && cantidadLibrosValido) {
+        console.log("Se ha enviado con éxito");
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function validarCampoNombre(pCampoNombre) {
+    let avisoNombre = document.getElementById("aviso_nombre");
     if (pCampoNombre.value === "") {
-        let avisoNombre = document.createElement("p");
-        avisoNombre.innerHTML = "El campo del Nombre es obligatorio";
-        avisoNombre.style.color = "orange";
-        pCampoNombre.insertAdjacentElement("afterend", avisoNombre);
+        if (!avisoNombre) {
+            avisoNombre = document.createElement("p");
+            avisoNombre.id = "aviso_nombre";
+            avisoNombre.innerHTML = "El campo del Nombre es obligatorio";
+            avisoNombre.style.color = "orange";
+            pCampoNombre.insertAdjacentElement("afterend", avisoNombre);
+        }
+        return false;
+    } else {
+        if (avisoNombre) {
+            avisoNombre.remove();
+        }
+        return true;
     }
 }
 
-function validarCampoLibros(pCampoLibros, pDivLibros) {
-    if (pCampoLibros.value === "") {
-        let avisoLibros = document.createElement("p");
-        avisoLibros.innerHTML = "- El campo del Libro es obligatorio";
-        avisoLibros.style.color = "orange";
-        pDivLibros.insertAdjacentElement("afterend", avisoLibros);
-        return avisoLibros;
-    }
-    return null;
-}
-
-function validarCampoCantidadLibros(pCampoCantidad, pErrorLibros) {
-    if (pCampoCantidad.value < 1 || pCampoCantidad.value > 5) {
-        console.log("Cantidad incorrecta");
-        let avisoCantidadLibros = document.createElement("p");
-        avisoCantidadLibros.innerHTML = "- La cantidad debe ser un número del 1 al 5";
-        avisoCantidadLibros.style.color = "orange";
-        pErrorLibros.insertAdjacentElement("afterend", avisoCantidadLibros);
-    }
-}
-
-function validarCampoTel(pCampoTel){
-  var formato=/^(\d{3}[- ]{1}){2}\d{3}$/;
-  //d= 0-9-ko zenbakia
-    if (!formato.test(pCampoTel.value)){
-        let avisoTel = document.createElement("p");
-        avisoTel.innerHTML = "El campo del telefono debe contener el patrón 123(-)456(-)789";
-        avisoTel.style.color = "orange";
-        pCampoTel.insertAdjacentElement("afterend", avisoTel);
+function validarCampoTel(pCampoTel) {
+    let formato = /^(\d{3}[- ]{1}){2}\d{3}$/;
+    let avisoTel = document.getElementById("aviso_tel");
+    if (!formato.test(pCampoTel.value)) {
+        if (!avisoTel) {
+            let avisoTel = document.createElement("p");
+            avisoTel.id = "aviso_tel";
+            avisoTel.innerHTML = "El campo del telefono debe contener el patrón 123(-)456(-)789";
+            avisoTel.style.color = "orange";
+            pCampoTel.insertAdjacentElement("afterend", avisoTel);
+        }
+        return false;
+    } else {
+        if (avisoTel) {
+            avisoTel.remove();
+        }
+        return true;
     }
 }
 
 function validarCampoEmail(pCampoEmail){
     var formato= /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
-    //w=a-z,A-Z eta 0-9     +=1 edo 1>  ?=0/1 *=0 edo 0>
-      if (!formato.test(pCampoEmail.value)){
-          let avisoEmail = document.createElement("p");
-          avisoEmail.innerHTML = "El campo del email es incorrecto. Debe tener el siguiente formato:hola@gmail.com";
-          avisoEmail.style.color = "orange";
-          pCampoEmail.insertAdjacentElement("afterend", avisoEmail);
-      }
-  }
+    let avisoEmail = document.getElementById("aviso_email");
+    if (!formato.test(pCampoEmail.value)) {
+        if (!avisoEmail) {
+            let avisoEmail = document.createElement("p");
+            avisoEmail.id = "aviso_email";
+            avisoEmail.innerHTML = "El campo del email es incorrecto. Debe tener el siguiente formato: hola@gmail.com";
+            avisoEmail.style.color = "orange";
+            pCampoEmail.insertAdjacentElement("afterend", avisoEmail);
+        }
+        return false;
+    } else {
+        if (avisoEmail) {
+            avisoEmail.remove();
+        }
+        return true;
+    }
+}
+
+function validarCampoLibros(pCampoLibros, pDivLibros) {
+    let avisoLibros = document.getElementById("aviso_libros");
+    if (pCampoLibros.value === "") {
+        if (!avisoLibros) {
+            let avisoLibros = document.createElement("p");
+            avisoLibros.id = "aviso_libros";
+            avisoLibros.innerHTML = "- El campo del Libro es obligatorio";
+            avisoLibros.style.color = "orange";
+            pDivLibros.insertAdjacentElement("afterend", avisoLibros);
+        }
+        return false;
+    } else {
+        if (avisoLibros) {
+            avisoLibros.remove();
+        }
+        return true;
+    }
+}
+
+function validarCampoCantidadLibros(pCampoCantidad, pLibrosValidos, pDivLibros) {
+    let avisoCantidadLibros = document.getElementById("aviso_cantidad_libros");
+    if (pCampoCantidad.value < 1 || pCampoCantidad.value > 5) {
+        if (!avisoCantidadLibros) {
+            let avisoCantidadLibros = document.createElement("p");
+            avisoCantidadLibros.id = "aviso_cantidad_libros";
+            avisoCantidadLibros.innerHTML = "- La cantidad debe ser un número del 1 al 5";
+            avisoCantidadLibros.style.color = "orange";
+            if (!pLibrosValidos) {
+                let avisoLibros = document.getElementById("aviso_libros");
+                avisoLibros.insertAdjacentElement("afterend", avisoCantidadLibros);
+            } else {
+                pDivLibros.insertAdjacentElement("afterend", avisoCantidadLibros);
+            }
+        }
+        return false;
+    } else {
+        if (avisoCantidadLibros) {
+            avisoCantidadLibros.remove();
+        }
+        return true;
+    }
+}
