e74f276284f056d0dd01b198fd6188b23c848f09
miribg <bergazm@gmail.com>
2023-03-28 18:34:51 +0200

    telefono validado

--- formulario.js
+++ formulario.js
@@ -8,8 +8,10 @@ function validacion(){
     let campoLibros = document.getElementById("campo_libros");
     let divLibros = document.getElementById("div_libros");
     let campoCantidadLibros = document.getElementById("campo_cantidad");
+    let campoTel= document.getElementById("campo_telefono");
 
     validarCampoNombre(campoNombre);
+    validarCampoTel(campoTel);
     let pErrorLibros = validarCampoLibros(campoLibros, divLibros);
     validarCampoCantidadLibros(campoCantidadLibros, pErrorLibros);
 
@@ -43,5 +45,15 @@ function validarCampoCantidadLibros(pCampoCantidad, pErrorLibros) {
         avisoCantidadLibros.innerHTML = "- La cantidad debe ser un número del 1 al 5";
         avisoCantidadLibros.style.color = "orange";
         pErrorLibros.insertAdjacentElement("afterend", avisoCantidadLibros);
+    }
+}
+
+function validarCampoTel(pCampoTel){
+  var formato=/^(\d{3}[-]?){2}\d{3}$/;
+    if (!formato.test(pCampoTel.value)){
+        let avisoTel = document.createElement("p");
+        avisoTel.innerHTML = "El campo del telefono debe contener el patrón 123(-)456(-)789";
+        avisoTel.style.color = "orange";
+        pCampoTel.insertAdjacentElement("afterend", avisoTel);
     }
 }
