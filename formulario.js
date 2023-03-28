6317ba5ec2793227cd5db8875f7f4c8a60faee10
Julen <jfuentes019@ikasle.ehu.eus>
2023-03-28 15:27:58 +0200

    Validacion nombre hecho

--- formulario.js
+++ formulario.js
@@ -3,10 +3,19 @@
 function validacion(){
     var textNomb = document.getElementById("campo_nombre").value;
     var textLib = document.getElementById("campo_libros").value;
-    if (textNomb === '' ){
-        console.log('error: vacio');
-        alert("Error, vacio");
-        return false;
+
+    let campoNombre = document.getElementById("campo_nombre");
+    validarNombreCliente(campoNombre);
+
+    return textNomb !== '';
+
+}
+
+function validarNombreCliente(pCampoNombre) {
+    if (pCampoNombre.value === "") {
+        console.log("Campo Nombre VACÍO");
+        let avisoNombre = document.createElement("p");
+        avisoNombre.innerHTML = "El campo del Nombre es obligatorio";
+        pCampoNombre.insertAdjacentElement("afterend", avisoNombre);
     }
-    return true;
 }
